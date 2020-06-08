83ea26c2610fe26070b2f0fb8ba1807c1ca353f9
Anastasiya Shadrina <a.shadrina5@mail.ru>
2020-06-08 16:15:31 +0700

    Add logging

--- lab1/src/main/java/ru/nsu/distributed/Main.java
+++ lab1/src/main/java/ru/nsu/distributed/Main.java
@@ -1,7 +1,13 @@
 package ru.nsu.distributed;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Logging works");
         System.out.println("Hello world!");
     }
 }
